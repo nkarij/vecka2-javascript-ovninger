437a6bb443c47877f5753e35a2646308eff42a20
Nanna <5608562@RTS-365.dk>
2019-09-04 15:16:57 +0200

    Ovning 2 - funktioner
    get input from input type text

--- 2-funktioner/scripts.js
+++ 2-funktioner/scripts.js
@@ -1,4 +1,21 @@
 
 // ØV2 - FUNKTIONER
 
+let buttonSubmit = document.querySelector(".submit");
+let textInput = document.querySelector(".input");
 
+buttonSubmit.addEventListener('click', (event) => {
+    event.preventDefault();
+    let upperCaseString = capitalizeInput(textInput.value);
+    // returnOutput er unødvendig, men bra practice :=)
+    returnOutput(upperCaseString);
+})
+
+function capitalizeInput(string){
+    let output = string.toUpperCase();
+    return output;
+}
+
+function returnOutput(string) {
+    textInput.value = string;
+}
